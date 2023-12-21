abcd2a27f0bcff6d690cde3abddc1efaecba3890
Jessica Wastyn <[jessica.wastyn@outlook.fr] (mailto:jessica.wastyn@outlook.fr)>
2023-12-21 14:06:55 +0100

    tipos de datos

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-
 <?php
-    echo "Hola mundo";
-?>
+//Mi primer hola mundo
+echo "Hola mundo";
+#Mi segundo hola mundo
+echo "Hola mundo dos";
+/**
+ * Comentario de multiples notas o comentarios
+ */
+echo "Hola mundo tres";
+
+
---
+++ tipoDatos.php
@@ -0,0 +1,9 @@
+<?php
+echo true;
+echo false;
+echo -10;
+var_dump(true);
+var_dump(false);
+var_dump(-10);
+var_dump(-10.7);
+var_dump('Hola chanchito chiquito');
