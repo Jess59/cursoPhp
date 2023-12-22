8a63c770ddbe58a264db481fad70f92b3c122671
Jessica Wastyn <[jessica.wastyn@outlook.fr] (mailto:jessica.wastyn@outlook.fr)>
2023-12-22 19:20:29 +0100

    constantes

--- constantes.php
+++ constantes.php
@@ -1,8 +1,21 @@
 <?php
-define("NOMBRE","Jessica Wastyn");
-const FULLNAME = "Jessica Aminta Ojeda Tezoquipa";
+define("NOMBRE","Pepito Pérez");
+const FULLNAME = "Pepito Pérez Lopez";
 echo NOMBRE;
 echo FULLNAME;
-const NOMBRE=10;
-echo NOMBRE;
+echo __FILE__;
+//resultat: C:\xampp\htdocs\cursoPhp\constantes.php
+echo __DIR__;
+//resultat: C:\xampp\htdocs\cursoPhp
+define('ANIMALES',array(
+    'chacal',
+    'cuervo',
+    'lagarto',
+    'marmota',
+    'pichon'
+));
+echo ANIMALES[1];
+
+
+
 ?>
